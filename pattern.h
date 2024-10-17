1b1340d8938aee0b28377019f8b17302ea51eeb1
mlhum <c1009080@newcastle.ac.uk>
2024-10-17 03:40:31 +0100

    added arrays to header which have static and oscillator patterns in

--- pattern.h
+++ pattern.h
@@ -4,23 +4,29 @@
 
 
 class Pattern {
-
+public:
+    int truePattern[][];
 };
 
 class Block : public Pattern {
-
+public:
+    int truePattern[3][2] = { {1, 0}, {0, 1}, {1, 1} };
 };
 
 class Beehive : public Pattern {
-
+public:
+    int truePattern[5][2] = { {1, 1}, {1, -1}, {2, 1}, {2, -1}, {3,0} };
 };
 
 class Blinker : public Pattern {
-
+public:
+    int truePattern[2][2] = {{1,0},{-1,0}};
 };
 
 class Toad : public Pattern {
-
+public:
+    int truePattern1[5][2] = { {1, 0}, {2, 0}, {1, 1}, {2, 1}, {3,1} };
+    int truePattern2[5][2] = { {0, 1}, {1, -1}, {3, 0}, {3, 1}, {2, 2} };
 };
 
 class Glider : public Pattern {
